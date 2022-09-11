3048be2effca3502c794b9c996c486fccd3b349a
DavidFFDAW <davidferflo2@gmail.com>
2022-09-11 18:44:08 +0200

    fix: fixed display of home module

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { NavigationComponent } from '../navigation/navigation.component';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/modules/shared.module';
 import { MaterialModule } from 'src/app/shared/modules/material.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent,
-    NavigationComponent,
     HomeComponent
   ],
   imports: [
     SharedModule,
-    MaterialModule
+    MaterialModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
