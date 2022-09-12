d5a16438ef5a3f1a565ab5a473170d576c7b585f
DavidFFDAW <davidferflo2@gmail.com>
2022-09-12 18:44:06 +0200

    ci(docker.sh): build will not output anything to the console when executed

--- docker.sh
+++ docker.sh
@@ -2,6 +2,6 @@ docker container rm -f angular_champions_app
 
 docker image rm -f angular_champions
 
-docker build -t angular_champions .
+docker build -t angular_champions -q .
 
-docker run -d -i -t -p 4200:4200 --name=angular_champions_app react-plants
+docker run -d -i -t -p 4200:80 --name=angular_champions_app react-plants
