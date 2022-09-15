ce921aa7c60db578554848ae19a820b470a31e33
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-09-15 08:04:55 +0200

    fix(HomeModule): typo extra comma in imports instruction

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -14,7 +14,7 @@ import { MatCardModule } from '@angular/material/card';
   ],
   imports: [
     SharedModule,
-    MaterialModule,,
+    MaterialModule,
     MatButtonModule,
     MatCardModule,
     HomeRoutingModule
