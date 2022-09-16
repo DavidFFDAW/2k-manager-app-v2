d0d98de3704834fea6ebd32ce1202bd2ddf71d81
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-09-16 13:58:03 +0200

    fix(AuthModule): solved unterminated str literal typo error

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { SharedModule } from 'src/app/shared/modules/shared.module';
 import { LoginRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input
+import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RegisterComponent } from './register/register.component';
