c8cf7c1e8138370f3f608386ef82b022ab76f298
David Fernández Flores <56683644+DavidFFDAW@users.noreply.github.com>
2022-09-16 13:54:31 +0200

    refactor(environment): updated API endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'https://vps-f87b433e.vps.ovh.net:8626/api/v1/',
+  API_ENDPOINT: 'https://vps-f87b433e.vps.ovh.net/2k/api/v2/',
 };
